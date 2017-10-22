b801f427b24f205c128e4234664b8afa28200e12
u1087117 <u1087117@utah.edu>
2017-10-22 14:50:20 -0600

    Added: Mesh implementation (OpenGL only).\nUpdated: Graphics accepts a mesh-effect pair to be submitted.

---
+++ Engine/Graphics/cMesh.cpp
@@ -0,0 +1,122 @@
+// Include Files
+//==============
+
+#include "cMesh.h"
+
+#include "VertexFormats.h"
+
+#include <Engine/Asserts/Asserts.h>
+#include <Engine/Graphics/sColor.h>
+#include <Engine/Logging/Logging.h>
+#include <Engine/Math/sVector.h>
+
+#include <new>
+
+// Interface
+//==========
+
+// Initialization / Clean Up
+//--------------------------
+
+eae6320::cResult eae6320::Graphics::cMesh::Create(cMesh*& o_mesh, const uint16_t i_vertexCount, const eae6320::Math::sVector* i_vertices, const eae6320::Graphics::sColor* i_colors)
+{
+    auto result = Results::Success;
+
+    // Validate inputs
+    {
+        if (i_vertexCount % s_verticesPerTriangle)
+        {
+            result = Results::Failure;
+            EAE6320_ASSERTF(false, "The vertex count for a mesh must be a multiple of 3!");
+            Logging::OutputError("The vertex count for a mesh must be a multiple of 3!");
+            goto OnExit;
+        }
+        else if (i_vertices == nullptr)
+        {
+            result = Results::Failure;
+            EAE6320_ASSERTF(false, "Invalid vertex data!");
+            Logging::OutputError("Invalid vertex data!");
+            goto OnExit;
+        }
+        else if (i_colors == nullptr)
+        {
+            result = Results::Failure;
+            EAE6320_ASSERTF(false, "Invalid color data!");
+            Logging::OutputError("Invalid color data!");
+            goto OnExit;
+        }
+    }
+
+    cMesh* newMesh = nullptr;
+
+    // Allocate a new mesh
+    {
+        newMesh = new (std::nothrow) cMesh();
+        if (newMesh == nullptr)
+        {
+            result = Results::OutOfMemory;
+            EAE6320_ASSERTF(false, "Couldn't allocate memory for the new mesh!");
+            Logging::OutputError("Failed to allocated memory for the new mesh!");
+            goto OnExit;
+        }
+    }
+
+    // Initialize the new mesh's geometry
+    if (!(result = newMesh->Initialize(i_vertexCount, i_vertices, i_colors)))
+    {
+        EAE6320_ASSERTF(false, "Could not initialize the new mesh!");
+        goto OnExit;
+    }
+
+OnExit:
+
+    if (result)
+    {
+        EAE6320_ASSERT(newMesh);
+        o_mesh = newMesh;
+    }
+    else
+    {
+        if (newMesh)
+        {
+            newMesh->DecrementReferenceCount();
+            newMesh = nullptr;
+        }
+        o_mesh = nullptr;
+    }
+
+    return result;
+}
+
+eae6320::Graphics::cMesh::~cMesh()
+{
+    CleanUp();
+}
+
+// Implementation
+//===============
+
+void eae6320::Graphics::cMesh::GetVertexBufferData(VertexFormats::sMesh* o_vertexData, const uint16_t i_vertexCount, const eae6320::Math::sVector* i_vertices, const eae6320::Graphics::sColor* i_colors) const
+{
+    EAE6320_ASSERT(o_vertexData != nullptr && i_vertexCount % s_verticesPerTriangle == 0);
+
+    for (uint16_t i = 0; i < i_vertexCount; ++i)
+    {
+        o_vertexData[i].x = i_vertices[i].x;
+        o_vertexData[i].y = i_vertices[i].y;
+        o_vertexData[i].r = static_cast<uint8_t>(i_colors[i].r * 255.0f);
+        o_vertexData[i].g = static_cast<uint8_t>(i_colors[i].g * 255.0f);
+        o_vertexData[i].b = static_cast<uint8_t>(i_colors[i].b * 255.0f);
+        o_vertexData[i].a = static_cast<uint8_t>(i_colors[i].a * 255.0f);
+    }
+}
+
+void eae6320::Graphics::cMesh::GetIndexBufferData(uint16_t* o_indexData, const uint16_t i_vertexCount, const eae6320::Math::sVector* i_vertices) const
+{
+    EAE6320_ASSERT(o_indexData != nullptr && i_vertexCount % s_verticesPerTriangle == 0);
+
+    for (uint16_t i = 0; i < i_vertexCount; ++i)
+    {
+        o_indexData[i] = i;
+    }
+}
---
+++ Engine/Graphics/cMesh.h
@@ -0,0 +1,129 @@
+/*
+    TODO
+*/
+
+#ifndef EAE6320_GRAPHICS_CMESH_H
+#define EAE6320_GRAPHICS_CMESH_H
+
+// Include Files
+//==============
+
+#include <Engine/Assets/ReferenceCountedAssets.h>
+#include <Engine/Results/Results.h>
+
+#ifdef EAE6320_PLATFORM_GL
+#include "OpenGL/Includes.h"
+#endif
+
+// Forward Declarations
+//=====================
+
+#ifdef EAE6320_PLATFORM_D3D
+struct ID3D11Buffer;
+struct ID3D11InputLayout;
+#endif
+
+namespace eae6320
+{
+    namespace Graphics
+    {
+        namespace VertexFormats
+        {
+            struct sMesh;
+        }
+        struct sColor;
+    }
+
+    namespace Math
+    {
+        struct sVector;
+    }
+}
+
+// Class Declaration
+//==================
+
+namespace eae6320
+{
+    namespace Graphics
+    {
+        class cMesh
+        {
+            // Interface
+            //==========
+
+        public:
+
+            // Render
+            //-------
+
+            void Draw() const;
+
+            // Initialization / Clean Up
+            //--------------------------
+
+            static cResult Create(cMesh*& o_mesh, const uint16_t i_vertexCount, const eae6320::Math::sVector* i_vertices, const eae6320::Graphics::sColor* i_colors);
+
+        public:
+
+            // Reference Counting
+            //-------------------
+
+            EAE6320_ASSETS_DECLAREREFERENCECOUNTINGFUNCTIONS();
+
+            EAE6320_ASSETS_DECLAREDELETEDREFERENCECOUNTEDFUNCTIONS(cMesh);
+
+        private:
+
+            cResult Initialize(const uint16_t i_vertexCount, const eae6320::Math::sVector* i_vertices, const eae6320::Graphics::sColor* i_colors);
+            cResult CleanUp();
+
+            cMesh() = default;
+            ~cMesh();
+
+        private:
+
+            // Implementation
+            //===============
+
+            void GetVertexBufferData(VertexFormats::sMesh* o_vertexData, const uint16_t i_vertexCount, const eae6320::Math::sVector* i_vertices, const eae6320::Graphics::sColor* i_colors) const;
+
+            void GetIndexBufferData(uint16_t* o_indexData, const uint16_t i_vertexCount, const eae6320::Math::sVector* i_vertices) const;
+
+        private:
+
+            // Data
+            //=====
+
+            static const uint8_t s_verticesPerTriangle = 3;
+
+#if defined( EAE6320_PLATFORM_D3D )
+            // A vertex buffer holds the data for each vertex
+            ID3D11Buffer* m_vertexBuffer = nullptr;
+            // An index buffer holds the indices to vertex data
+            ID3D11Buffer* m_indexBuffer = nullptr;
+            // D3D has an "input layout" object that associates the layout of the vertex format struct
+            // with the input from a vertex shader
+            ID3D11InputLayout* m_vertexInputLayout = nullptr;
+#endif
+
+            uint16_t m_indexCount = 0;
+
+            EAE6320_ASSETS_DECLAREREFERENCECOUNT();
+
+#if defined( EAE6320_PLATFORM_GL )
+            // A vertex buffer holds the data for each vertex
+            GLuint m_vertexBufferId = 0;
+            // An index buffer holds the indices to vertex data
+            GLuint m_indexBufferId = 0;
+            // A vertex array encapsulates the vertex data as well as the vertex input layout
+            GLuint m_vertexArrayId = 0;
+#endif
+
+        }; // class cMesh
+
+    } // namespace Graphics
+
+} // namespace eae6320
+
+#endif // EAE6320_GRAPHICS_CMESH_H
